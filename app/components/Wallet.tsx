c4d669fbb4d77dc4acf02d716aa7d8bb7ca57167
JanmejayR <JayRathore.1010@gmail.com>
2025-04-15 07:43:12 +0530

    initial layout set

---
+++ app/components/Wallet.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+const Wallet = () => {
+  return (
+    <div>
+        <Button>Generate Wallet</Button>
+    </div>
+  )
+}
+
+export default Wallet
